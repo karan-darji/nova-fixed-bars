ec5768b8cb407a17c9504ac1fa9e04ac2fe5ca2b
Parth KaPatel <parth.kapatel@arsenaltech.com>
2023-02-10 13:09:43 +0530

    update nova fixed bar for laravel nova 4

--- resources/js/dynamicNavbar.js
+++ resources/js/dynamicNavbar.js
@@ -18,30 +18,6 @@ function calibrateWidths() {
             'width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0'
         )
 
-    let sidebar = document.querySelector('.w-sidebar')
-    let content = document.querySelector('#nova .content')
     let contentHeader = document.querySelector('#nova .h-header')
-    let allNovaDivs = document.querySelectorAll('#nova > div');
-    if(allNovaDivs.length > 0){
-        if(contentHeader == null && allNovaDivs[0] != undefined){
-            allNovaDivs[0].className += ' h-header';
-            contentHeader = document.querySelector('#nova .h-header')
-        }
-        if(allNovaDivs[1] != undefined){
-            if(sidebar == null){
-                let sidebarChildren = allNovaDivs[1].children;
-                if(sidebarChildren != undefined && sidebarChildren.length > 0){
-                    sidebarChildren[0].className += ' w-sidebar ';
-                }
-            }
-            if(content == null){
-                let contentChildren = allNovaDivs[1].children;
-                if(contentChildren != undefined && contentChildren.length > 0){
-                    contentChildren[1].className += ' content ';
-                }
-            }
-        }
-    }
-
     contentHeader.style.width = '100%'
 }
